a73ea313752008abf07066c4cbfae0483ce977af
Ashish <ashu070020@gmail.com>
2022-01-22 17:44:45 +0530

    Half work

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,12 @@
 <?php
 require_once 'vendor/autoload.php';
+require_once 'LightOpenID/openid.php';
+
+// Include required libraries
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '../.env');
 $dotenv->load();
 
@@ -15,14 +22,19 @@ define('FACEBOOK_APP_ID', $_ENV['facbook_app_id']);
 define('FACEBOOK_APP_SECRET', $_ENV['facbook_app_secret']);
 define('FACEBOOK_REDIRECT_URL', 'https://localhost/social_login/index.php');
 
+// google crediantial
+define('GOOGLE_CLIENT_ID', $_ENV['google_client_id']);
+define('GOOGLE_CLIENT_SECRET', $_ENV['google_client_secret']);
+define('GOOGLE_REDIRECT_URL', 'https://localhost/social_login/index.php');
+
+// yahoo crediantial
+define('YAHOO_CLIENT_ID', $_ENV['yahoo_client_id']);
+define('YAHOO_CLIENT_SECRET', $_ENV['yahoo_client_secret']);
+define('YAHOO_REDIRECT_URL', 'https://localhost/social_login/dashboard.php');
+
 if(!session_id()) {
     session_start();
 }
-
-// Include required libraries
-use Facebook\Facebook;
-use Facebook\Exceptions\FacebookResponseException;
-use Facebook\Exceptions\FacebookSDKException;
 
 // Call Facebook API
 $fb = new Facebook(array(
@@ -43,7 +55,7 @@ try {
     if(isset($_SESSION['facebook_access_token'])){
         $accessToken = $_SESSION['facebook_access_token'];
     }else{
-        $accessToken = $helper->getAccessToken();
+        //$accessToken = $helper->getAccessToken();
     }
 }catch(FacebookResponseException $e) {
     echo 'Graph returned an error: ' . $e->getMessage();
@@ -52,3 +64,31 @@ try {
     echo 'Facebook SDK returned an error: ' . $e->getMessage();
     exit;
 }
+
+// Call Google API
+$gClient = new Google_Client();
+$gClient->setApplicationName('Social Login');
+$gClient->setClientId(GOOGLE_CLIENT_ID);
+$gClient->setClientSecret(GOOGLE_CLIENT_SECRET);
+$gClient->setRedirectUri(GOOGLE_REDIRECT_URL);
+$gClient->addScope("email");
+$gClient->addScope("profile");
+$google_oauth = new Google_Service_Oauth2($gClient);
+
+//yahoo API
+try{
+    if(!isset($_SESSION["yahoo_login"])){
+        $url = "https://api.login.yahoo.com/oauth2/request_auth";
+        $param = array(
+                    'client_id'     => YAHOO_CLIENT_ID,
+                    'response_type' => 'code',
+                    'redirect_uri'  => YAHOO_REDIRECT_URL,
+                    'scope'         => 'email,mail-r',
+                    'nonce'         =>  'YihsFwGKgt3KJUh6tPs2'
+                );
+    }
+}catch(Exception $ex){
+    echo $ex->getMessage();
+}
+
+
--- index.php
+++ index.php
@@ -81,14 +81,111 @@ if(isset($accessToken)){
     }else{
         $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
     }
+}else if(isset($_GET['code'])){
+    $token = $gClient->fetchAccessTokenWithAuthCode($_GET['code']);
+    $_SESSION['google_token'] = $token;
+
+    if (isset($_SESSION['google_token'])) {
+        $gClient->setAccessToken($_SESSION['google_token']);
+    }
+    
+     
+    if(isset($_SESSION['google_token'])){ 
+        $gClient->setAccessToken($_SESSION['google_token']); 
+    } 
+     
+    if ($gClient->getAccessToken()) { 
+        // Get user profile data from google 
+        $gpUserProfile = $google_oauth->userinfo->get();
+         
+        // Initialize User class 
+        $user = new User(); 
+         
+        // Getting user profile info 
+        $gpUserData = array(); 
+        $gpUserData['oauth_uid']  = !empty($gpUserProfile['id'])?$gpUserProfile['id']:''; 
+        $gpUserData['first_name'] = !empty($gpUserProfile['given_name'])?$gpUserProfile['given_name']:''; 
+        $gpUserData['last_name']  = !empty($gpUserProfile['family_name'])?$gpUserProfile['family_name']:''; 
+        $gpUserData['email']      = !empty($gpUserProfile['email'])?$gpUserProfile['email']:''; 
+        $gpUserData['gender']     = !empty($gpUserProfile['gender'])?$gpUserProfile['gender']:''; 
+        $gpUserData['link']     = !empty($gpUserProfile['locale'])?$gpUserProfile['locale']:''; 
+        $gpUserData['picture']    = !empty($gpUserProfile['picture'])?$gpUserProfile['picture']:''; 
+         
+        // Insert or update user data to the database 
+        $gpUserData['oauth_provider'] = 'google'; 
+        $userData = $user->checkUser($gpUserData); 
+         
+        // Storing user data in the session 
+        $_SESSION['userData'] = $userData; 
+         
+        // Render user profile data 
+        if(!empty($userData)){ 
+            $output     = '<h2>Google Account Details</h2>'; 
+            $output .= '<div class="ac-data">'; 
+            $output .= '<img src="'.$userData['picture'].'">'; 
+            $output .= '<p><b>Google ID:</b> '.$userData['oauth_uid'].'</p>'; 
+            $output .= '<p><b>Name:</b> '.$userData['first_name'].' '.$userData['last_name'].'</p>'; 
+            $output .= '<p><b>Email:</b> '.$userData['email'].'</p>'; 
+            $output .= '<p><b>Gender:</b> '.$userData['gender'].'</p>'; 
+            $output .= '<p><b>Locale:</b> '.$userData['link'].'</p>'; 
+            $output .= '<p><b>Logged in with:</b> Google Account</p>'; 
+            $output .= '<p>Logout from <a href="logout.php">Google</a></p>'; 
+            $output .= '</div>'; 
+        }else{ 
+            $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>'; 
+        } 
+    }
+}else if(($yahooid->mode != "cancel") && $yahooid->mode){
+    if($yahooid->validate()){
+        $data = $yahooid->getAttributes();
+        $Identity = explode("=",$yahooid->identity);
+        $userid = $Identity[1];
+        $loginwith = 'Yahoo';
+
+        // Initialize User class
+        $user = new User();
+
+        $yahooUserData = array();
+        $yahooUserData['oauth_uid']  = !empty($userid)?$userid:'';
+        $yahooUserData['first_name'] = !empty($data['namePerson/first'])?$data['namePerson/first']:'';
+        $yahooUserData['last_name']  = !empty($data['namePerson/last'])?$data['namePerson/last']:'';
+        $yahooUserData['email']      = !empty($data['contact/email'])?$data['contact/email']:'';
+        $yahooUserData['gender']     = !empty($data['person/gender	'])?$data['person/gender']:'';
+        $yahooUserData['link']       = !empty($data['pref/language'])?$data['pref/language']:'';
+        
+        // Insert or update user data to the database
+        $yahooUserData['oauth_provider'] = 'yahoo';
+        $userData = $user->checkUser($yahooUserData);
+        $logoutURL = BASE_URL."logout";
+        if(!empty($userData)){
+            $output  = '<div class="col-md-12">';
+            $output .= '<h2>Yahoo Profile Details</h2>';
+            $output .= '<div class="ac-data">';
+            $output .= '<p><b>Yahoo ID:</b> '.$userData['oauth_uid'].'</p>';
+            $output .= '<p><b>Name:</b> '.$userData['first_name'].' '.$userData['last_name'].'</p>';
+            $output .= '<p><b>Email:</b> '.$userData['email'].'</p>';
+            $output .= '<p><b>Gender:</b> '.$userData['gender'].'</p>';
+            $output .= '<p><b>Logged in with:</b> Yahoo</p>';
+            $output .= '<p><b>Logout from <a href="'.$logoutURL.'">Yahoo</a></p>';
+            $output .= '</div>';
+            $output .= '</div>';
+        }else{
+            $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
+        }
+    }else{
+        $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
+    }
 }else{
     // Get login url
     $permissions = ['email']; // Optional permissions
     $loginURL = $helper->getLoginUrl(FACEBOOK_REDIRECT_URL, $permissions);
+    $authUrl = $gClient->createAuthUrl();
+    $google_url = filter_var($authUrl, FILTER_SANITIZE_URL);
+    $yahoo_url = BASE_URL."index.php?login=1";
     $output = "<div class=\"col-md-3\"><a href=\"$loginURL\" class=\"btn btn-primary btn-block mb-2\">Login with Facebook</a></div>";
-    $output .= "<div class=\"col-md-3\"><a href=\"#\" class=\"btn btn-danger btn-block mb-2\">Login with Google</a></div>";
+    $output .= "<div class=\"col-md-3\"><a href=\"$google_url\" class=\"btn btn-danger btn-block mb-2\">Login with Google</a></div>";
     $output .= "<div class=\"col-md-3\"><a href=\"#\" class=\"btn btn-info btn-block mb-2\">Login with Twitter</a></div>";
-    $output .= "<div class=\"col-md-3\"><a href=\"#\" class=\"btn btn-primary btn-block mb-2\">Login with Yahoo</a></div>";
+    $output .= "<div class=\"col-md-3\"><a href=\"$yahoo_url\" class=\"btn btn-primary btn-block mb-2\">Login with Yahoo</a></div>";
 }
 ?>
 
